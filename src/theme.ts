25585ef54f2627f3dd8e318e6d47dabdf1dfd8eb
multida <wow_da65@naver.com>
2024-10-25 18:12:25 +0900

    초기 세팅 및 api 호출 완

---
+++ src/theme.ts
@@ -0,0 +1,9 @@
+import { DefaultTheme } from "styled-components";
+
+const theme: DefaultTheme = {
+  bgColor: "#2f3640",
+  textColor: "#f5f6fa",
+  accentColor: "#44bd32",
+};
+
+export default theme;
